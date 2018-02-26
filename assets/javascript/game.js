7f1eab48e2e0040112c221c6940c4c6ca0de4be7
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-02-25 22:34:02 -0500

    changed win-lose message display time and reference words to be consistent with theme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,13 +8,13 @@ function reset() {
     userNumber = 0;
     $(".progress-bar").css("width", "0%");
     $("#userNumber").html(userNumber);
-    $(".crystals-collected").empty();
-    crystalValues();
+    $(".gems-collected").empty();
+    gemValues();
 }
 
-//give values to the crystals
-function crystalValues() {
-    $(".crystal").each(function() {
+//give values to the gems
+function gemValues() {
+    $(".gem").each(function() {
         $(this).attr("value", Math.floor(Math.random() * 12 +1));
         var currentValue = $(this).attr("value");
         console.log($(this).attr("alt") + " " + $(this).attr("value"));
@@ -26,14 +26,14 @@ function check() {
     if (userNumber === guessNumber) {
         var $wins = parseInt($("#wins").html()) + 1;
         $("#wins").html($wins);
-        $(".crystals-collected").append("<h1 class='win-lose-message' style='background: green'>You win!</h1>");
-        setTimeout(reset, 3000);
+        $(".gems-collected").append("<h1 class='win-lose-message' style='background: green'>You win!</h1>");
+        setTimeout(reset, 2000);
     }
     else if (userNumber > guessNumber) {
         var $losses = parseInt($("#losses").html()) + 1;
         $("#losses").html($losses);
-        $(".crystals-collected").append("<h1 class='win-lose-message' style='background: red'>You lose!</h1>");
-        setTimeout(reset, 3000);
+        $(".gems-collected").append("<h1 class='win-lose-message' style='background: red'>You lose!</h1>");
+        setTimeout(reset, 2000);
     }
 }
 
@@ -41,9 +41,9 @@ function check() {
 $(document).ready(function () {
 
     reset();
-    //When crystal is clicked, add the crystal to the the "collected crystals" section, add it's value to the user's number, and update the progress bar to reflect current status
-    $(".crystal").on("click", function () {
-        $(".crystals-collected").append("<img src=" + $(this).attr("src") + " max-height='100px' width='100px' class='crystal-collected'>");
+    //When gem is clicked, add the gem to the the "collected gems" section, add it's value to the user's number, and update the progress bar to reflect current status
+    $(".gem").on("click", function () {
+        $(".gems-collected").append("<img src=" + $(this).attr("src") + " max-height='100px' width='100px' class='gem-collected'>");
         userNumber += parseInt($(this).attr("value"));
         $(".progress-bar").css("width", (userNumber / parseInt(guessNumber) * 100) + "%");
         $("#userNumber").html(userNumber);
